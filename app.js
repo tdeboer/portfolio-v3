1b8fe08f4093d5630cd6b0c8b56af4bc7d8f27f4
tdeboer <deboer.t@gmail.com>
2017-06-08 20:42:46 +0200

    Favicon

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(helmet());
-app.use(favicon(path.join(__dirname, 'public/src/images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/src/images/favicons', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
